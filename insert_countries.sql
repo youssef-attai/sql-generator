77c44bcb69de800c412c01aa1dd2bf744718ad1d
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-05-06 16:52:07 +0300

    add inserts

---
+++ insert_countries.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO [dbo].[Country]
+	(Country) VALUES 
+	('United States'), 
+	('Canada'), 
+	('United Kingdom'), 
+	('France'), 
+	('Germany'), 
+	('Italy'), 
+	('Spain'), 
+	('Australia'), 
+	('Japan'), 
+	('China'), 
+	('Brazil'), 
+	('Mexico'), 
+	('Argentina'), 
+	('South Africa'), 
+	('Nigeria'), 
+	('Russia'), 
+	('India'), 
+	('Pakistan'), 
+	('Turkey'), 
+	('Egypt');
+
